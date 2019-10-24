1a95cf74d4fe373afd43b40fa6535776ee5758ad
Victoriano Alfonso Phocco Diaz <victoriano.alfonso@semantix.com.br>
2019-10-23 23:34:54 -0300

    Fixing and improving attributes getting and saving.

--- src/mapping.py
+++ src/mapping.py
@@ -10,21 +10,23 @@ import codecs
 
 def get_scheduling(job_xml):
     try:
-        return '' if not job_xml.job.scheduleEnabled.pyval else ';'.join([ str(sch.attrib) for sch in job_xml.job.schedule.getchildren()])
-    except Exception as error:
-        return 'No Scheduling definition'
+        return 'no-scheduled' if not job_xml.job.find('schedule') else ';'.join([ str(sch.attrib) for sch in job_xml.job.schedule.getchildren()])
+    except Exception as e:
+        print(str(e))
+        return 'No Scheduling definition found'
 
 def get_job_definition(rundeck_client, job_id):
     response = rundeck_client.export_job(job_id)
     job_xml = objectify.fromstring(response.text)
     print("scheduleEnabled:" + str(job_xml.job.scheduleEnabled.pyval))
+    print("executionEnabled:" + str(job_xml.job.executionEnabled.pyval))
     job_definition = {
-        'group': job_xml.job.group.text if job_xml.job.find('group') else 'no group',
+        'group': job_xml.job.group.text if job_xml.job.find('group') else 'no-group',
         'executionEnabled': job_xml.job.executionEnabled.pyval,
         'scheduleEnabled': job_xml.job.scheduleEnabled.pyval,
         'schedule': get_scheduling(job_xml),
         'steps_strategy': job_xml.job.sequence.get('strategy'),
-        'nodefilter': job_xml.job.nodefilters.filter.text.strip() if job_xml.job.find('nodefilters') else 'no filter'
+        'nodefilter': job_xml.job.nodefilters.filter.text.strip() if job_xml.job.find('nodefilters') else 'no-filter'
     }
     if job_xml.job.sequence.countchildren() > 0:
         job_definition['steps'] = [ get_job_step_info(xml_step) for xml_step in job_xml.job.sequence.getchildren() ]
@@ -36,28 +38,36 @@ def get_job_step_info(xml_step):
     try:
         xml_step_definition = xml_step.getchildren()
         #if len(xml_step_definition)
-        if xml_step_definition[1].tag == "jobref":
+        has_description = xml_step.find('description')
+        if xml_step_definition[1 if has_description else 0].tag == "jobref":
             return {
-                'description': xml_step.description.text,
+                'description': xml_step.description.text if has_description else 'no-description',
                 'type': 'jobref',
                 'target': xml_step.jobref.get('name'),
-                'group': xml_step.jobref.get('group')
+                'group':  xml_step.jobref.get('group') ,
+                'arg':    xml_step.arg.text if xml_step.find('args') else 'no-arg'
             }
-        elif xml_step_definition[1].tag == "script":
+        elif xml_step_definition[1 if has_description else 0].tag == "script":
             return {
                 'description': xml_step.description.text,
                 'type': 'script',
                 'target': xml_step.script.text,
-                'args': xml_step.scriptargs.text
+                'args': xml_step.scriptargs.text if xml_step.find('scriptargs') else 'no-args'
             }
         else:
             raise ValueError('Not supported type step %s' % xml_step_definition[1].tag)
     except Exception as e:
         print str(e)
-        return {}        
+        return {}
 
 def get_information_jobs(jobs, rundeck_client):
     return [ (job['project'], job['group'], job['name'], get_job_definition(rundeck_client, job['id'])) for job in jobs]
+
+def save(file_path, data_dict):
+    with codecs.open(file, "w", "utf-8") as f:
+        for child in data_dict:
+            f.write(str(child) + '\n')
+
 
 def mapping():
     config = configparser.ConfigParser()
@@ -73,8 +83,7 @@ def mapping():
     projects_jobs = [ rundeck.list_jobs(project['name']) for project in projects ]
     information_jobs = [ get_information_jobs(jobs, rundeck) for jobs in projects_jobs ]
 
-    with codecs.open("/home/alfonso/tmp/output.txt", "w", "utf-8") as f:
-        for info_job in information_jobs:
-            f.write(str(info_job) + '\n')
+    for info_job in information_jobs:
+        save("/home/alfonso/tmp/output"+ info_job[0]+".txt", info_job)
 
 mapping()
